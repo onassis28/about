b5c25fd2e15044e86eaf361839a87f3514bdf335
Onassis Sowah Anyetei <onassis.sowah@gmail.com>
2022-05-18 09:30:41 +0200

    ui updates

--- src/components/About/AboutItems.jsx
+++ src/components/About/AboutItems.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './about.css'
+import{AiFillLinkedin} from 'react-icons/ai'
+import{AiFillTwitterCircle} from 'react-icons/ai'
 
 const AboutItems = () => {
   return (
@@ -13,12 +15,11 @@ const AboutItems = () => {
             <li>Bullet points</li>
             <li>Bullet points</li>
             <li>Bullet points</li>
-            <li>Bullet points</li>
-            <li>Bullet points</li>
-            <li>Bullet points</li>
-            <li>Bullet points</li>
+        <div className="icons_container">
 
-
+        <a  href='https://linkedin.com'><AiFillLinkedin/></a>
+        <a  href='https://twitter.com'><AiFillTwitterCircle/></a>
+        </div>
 
         </ul>
         </article>
